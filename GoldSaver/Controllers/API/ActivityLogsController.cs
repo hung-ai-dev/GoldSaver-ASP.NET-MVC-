b20587a9f8c99a2e822d1cc7a1fb34cdf42ce84f
hungnds <ndhung.uet@gmail.com>
2016-10-25 15:59:12 +0700

    Get history transaction API and display

--- GoldSaver/Controllers/API/ActivityLogsController.cs
+++ GoldSaver/Controllers/API/ActivityLogsController.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Web.Http;
-using System.Web.Routing;
 using GoldSaver.Models;
 using Microsoft.AspNet.Identity;
 
@@ -35,6 +32,54 @@ namespace GoldSaver.Controllers.API
             _context.Transactions.Add(newTrans);
             _context.SaveChanges();
             return Ok();
+        }
+
+        [HttpGet]
+        public IEnumerable<Object> GetLog()
+        {
+            List<Object> trans = new List<Object>();
+            var userId = User.Identity.GetUserId();
+            var obj = _context.Transactions.Where(t => t.UserId == userId).Select(t => new
+            {
+                CategoryId = t.CategoryId,
+                Note = t.Note,
+                DateTime = t.DateTime,
+                Cost = t.Cost
+            }).GroupBy(t => t.DateTime).ToList();
+
+            var history = new List<Object>();
+
+            foreach (var item in Enumerable.Reverse(obj))
+            {
+                double sum = 0;
+                var listlog = new List<Object>();
+
+                foreach (var _item in item)
+                {
+                    listlog.Add(new
+                    {
+                        CategoryLink = _context.Categories.Where(c => c.Id == _item.CategoryId).Select(c => c.Link).ToList().First(),
+                        CategoryName = _context.Categories.Where(c => c.Id == _item.CategoryId).Select(c => c.CategoryName).ToList().First(),
+                        Note = _item.Note,
+                        Cost = _item.Cost,
+                    });
+                    sum += _item.Cost;
+                }
+
+                var newObj = new
+                {
+                    logweekday = item.First().DateTime.DayOfWeek.ToString(),
+                    logday = item.First().DateTime.Day,
+                    logmonth = item.First().DateTime.Month,
+                    logyear = item.First().DateTime.Year,
+                    listlog = listlog,
+                    cost = sum
+                };
+                history.Add(newObj);
+            }
+            
+
+            return history;
         }
     }
 }
